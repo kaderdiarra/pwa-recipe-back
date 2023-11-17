6ad150158e40f42a5d9f2f93264590bc8762bbaf
Kader Diarra <kader_invictus@Kaders-MacBook-Pro.local>
2023-11-17 22:29:15 +0100

    Edit cors option

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 //MIDDLEWARES
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
